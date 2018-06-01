06b5bb8f0a74a61eec5c143724c9f9b2c8197322
Abhinav Kotak <in.abhi9@gmail.com>
2018-06-01 10:38:03 +0530

    Init for drf 3.8 support with python 3.6
    Signed-off-by: Abhinav Kotak <in.abhi9@gmail.com>

---
+++ README.md
@@ -0,0 +1,16 @@
+drf-ext
+=======
+This library is intended to extend / change some default behaviour to 
+Django REST Framework
+
+## Installation
+
+    python setup.py install
+    
+the pip way
+
+    pip install drf-ext
+    
+## To Do:
+
+* Docs
---
+++ setup.py
@@ -0,0 +1,40 @@
+'''
+pytz setup script
+'''
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+me = 'Abhinav Kotak'
+memail = 'in.abhi9@gmail.com'
+
+packages = ['drf_ext', 'drf_ext.db']
+install_requires = open('requirements.txt', 'r').readlines()
+
+setup(
+    name='drf_ext',
+    version='3.8.1',
+    zip_safe=True,
+    description='Extensions to drf',
+    author=me,
+    author_email=memail,
+    maintainer=me,
+    maintainer_email=memail,
+    install_requires=install_requires,
+    url='https://github.com/inabhi9/drf-ext',
+    license=open('LICENSE', 'r').read(),
+    keywords=['djangorestframework'],
+    packages=packages,
+    platforms=['Independant'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
