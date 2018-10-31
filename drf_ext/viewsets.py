96ba51d1101b1f5ce387450d866b5d1db77bed91
Abhinav Kotak <in.abhi9@gmail.com>
2018-10-31 12:23:11 +0530

    Added attachment response to viewset
    It returns the file that triggers downloading directly into the browser
    
    Signed-off-by: Abhinav Kotak <in.abhi9@gmail.com>

--- drf_ext/viewsets.py
+++ drf_ext/viewsets.py
@@ -3,13 +3,47 @@
 ViewSet
 =======
 """
-from django.http import QueryDict
+import mimetypes
+import os
+
+from django.http import QueryDict, HttpResponse
 from rest_framework import generics as rf_generics, viewsets as rf_viewsets
 from rest_framework_extensions.mixins import NestedViewSetMixin as _NestedViewSetMixin
 
 
 class GenericAPIView(rf_generics.GenericAPIView):
-    pass
+    @classmethod
+    def attachment_response(cls, file, request, remove_file=True):
+        filename = os.path.basename(file)
+        with open(file, 'rb') as fp:
+            response = HttpResponse(fp.read())
+
+        type_, encoding = mimetypes.guess_type(file)
+        if type_ is None:
+            type_ = 'application/octet-stream'
+
+        response['Content-Type'] = type_
+        response['Content-Length'] = str(os.stat(file).st_size)
+        if encoding is not None:
+            response['Content-Encoding'] = encoding
+
+        # To inspect details for the below code, see http://greenbytes.de/tech/tc2231/
+        if u'WebKit' in request.META['HTTP_USER_AGENT']:
+            # Safari 3.0 and Chrome 2.0 accepts UTF-8 encoded string directly.
+            filename_header = 'filename=%s' % filename
+        elif u'MSIE' in request.META['HTTP_USER_AGENT']:
+            # IE does not support internationalized filename at all.
+            # It can only recognize internationalized URL, so we do the trick via routing rules.
+            filename_header = ''
+        else:
+            # For others like Firefox, we follow RFC2231 (encoding extension in HTTP headers).
+            filename_header = 'filename*=UTF-8\'\'%s' % filename
+
+        response['Content-Disposition'] = 'attachment; ' + filename_header
+        if remove_file:
+            os.unlink(file)
+
+        return response
 
 
 class GenericViewSet(GenericAPIView, rf_viewsets.GenericViewSet):
